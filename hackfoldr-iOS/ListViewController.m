679c7acb9a4d3b16859dc95005f47d112583a436
Superbil <superbil@gmail.com>
2014-08-31 13:58:13 +0800

    new ListViewController and setting button

---
+++ hackfoldr-iOS/ListViewController.m
@@ -0,0 +1,22 @@
+//
+//  ListViewController.m
+//  hackfoldr-iOS
+//
+//  Created by Superbil on 2014/8/31.
+//  Copyright (c) 2014年 org.superbil. All rights reserved.
+//
+
+#import "ListViewController.h"
+
+@interface ListViewController ()
+@property (nonatomic, strong) IBOutlet UIButton *settingButton;
+@end
+
+@implementation ListViewController
+
+- (IBAction)settingAction:(id)sender
+{
+    NSLog(@"setting button clicked");
+}
+
+@end
