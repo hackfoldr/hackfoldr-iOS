a82c071dbf0bff0064b214053678b2cbb174891e
Superbil <superbil@gmail.com>
2017-03-08 03:21:35 +0800

    Move hackfoldrPageKey method to category

--- hackfoldr-iOS/NSUserDefaults+DefaultHackfoldrPage.m
+++ hackfoldr-iOS/NSUserDefaults+DefaultHackfoldrPage.m
@@ -52,4 +52,20 @@ static NSString *kCurrentHackfoldrPage = @"Current Hackfoldr Page";
     [self removeObjectForKey:kCurrentHackfoldrPage];
 }
 
+- (NSString *)hackfoldrPageKey
+{
+    NSString *pageKey = [self stringOfCurrentHackfoldrPage];
+
+    if (!pageKey || pageKey.length == 0) {
+        NSString *defaultPage = @"hackfoldr-iOS";
+
+        [self setDefaultHackfoldrPage:defaultPage];
+        [self synchronize];
+
+        pageKey = [self stringOfCurrentHackfoldrPage];
+    }
+
+    return pageKey;
+}
+
 @end
